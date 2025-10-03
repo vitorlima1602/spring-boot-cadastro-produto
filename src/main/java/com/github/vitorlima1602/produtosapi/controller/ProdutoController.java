e622ddb90bd1256786ee1070a27ccba19dbdfa9f
Vitor Lima <vitor_lima1602@hotmail.com>
2025-10-03 08:55:20 -0300

    criação de busca por parâmetro

--- src/main/java/com/github/vitorlima1602/produtosapi/controller/ProdutoController.java
+++ src/main/java/com/github/vitorlima1602/produtosapi/controller/ProdutoController.java
@@ -4,6 +4,7 @@ import com.github.vitorlima1602.produtosapi.model.Produto;
 import com.github.vitorlima1602.produtosapi.repository.ProdutoRepository;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -41,5 +42,10 @@ public class ProdutoController {
         produto.setId(id);
         produtoRepository.save(produto);
         return produto;
+    }
+
+    @GetMapping
+    public List<Produto> buscar(@RequestParam("nome") String nome) {
+        return produtoRepository.findByNomeContainingIgnoreCase(nome);
     }
 }
--- src/main/java/com/github/vitorlima1602/produtosapi/repository/ProdutoRepository.java
+++ src/main/java/com/github/vitorlima1602/produtosapi/repository/ProdutoRepository.java
@@ -3,10 +3,14 @@ package com.github.vitorlima1602.produtosapi.repository;
 import com.github.vitorlima1602.produtosapi.model.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 // JpaRepository<T, ID> - T: Tipo da entidade, ID: Tipo do identificador da entidade
 // JpaRepository já possui os métodos básicos de CRUD (Create, Read, Update, Delete)
 // Não é necessário implementar a interface, o Spring Data JPA faz isso automaticamente
 // Exemplo de uso: produtoRepository.save(produto), produtoRepository.findById(id), produtoRepository.findAll(), produtoRepository.deleteById(id)
 public interface ProdutoRepository extends JpaRepository<Produto, String> {
-
+//     Método para buscar produtos pelo nome, ignorando maiúsculas e minúsculas
+//     O Spring Data JPA interpreta o nome do método e gera a consulta automaticamente
+    List<Produto> findByNomeContainingIgnoreCase(String nome);
 }
